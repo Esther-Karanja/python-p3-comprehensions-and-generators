c3efc1b794cebb63b647901e6bc16f66c8477007
Esther Karanja <est.karanja@gmail.com>
2023-10-05 16:41:10 -0700

    completed lab, all tests running

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+  new_list =[None if i%2!=0 else i for i in num_list if i%2==0]
+  return (new_list)
+
+return_evens([0, 1, 3, 5, 7, 8, 9])
+
 
 def make_exclamation(sentence_list):
-    pass
+  new_list=[None if i==None else i+'!' for i in sentence_list]
+  return (new_list)
+
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
